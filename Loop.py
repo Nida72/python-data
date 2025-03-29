7c04482fbbd6fa680b4ebedc6838e0209e3d1346
unknown <nidahassan.com@gmail.com>
2025-03-29 20:33:48 +0500

    Initial commit

--- Loop.py
+++ Loop.py
@@ -1,5 +1,5 @@
 #while loop
-i = 1
+"""i = 1
 while i < 6:
   print(i)
   i += 1
@@ -9,5 +9,35 @@ while i < 6:
   i += 1
   if i == 3:
     continue
-  print(i)
+  print(i)"""
 #eg
+age = 19
+while age > 18:
+    break
+    print('You can vote')
+#print number
+
+number = 4
+while number > 0:
+    number -= 1
+    if number == 3:
+     print(number)
+#another eg
+# Take user input
+number = int(input("enter the number: ")) 
+while number < 510 :  
+    # Find mod of 2
+    if number%2 == 0:  
+        print("The number "+str(number)+" is even")
+    else:
+        print("The number "+str(number)+" is odd")
+    number = number+1
+#another eg
+i = 3
+while(i < 60):
+ j = 3
+ while(j <= (i/j)):
+   if not(i%j): break
+   j = j + 1
+ if (j > i/j) : print (i, " is prime")
+ i = i + 1
--- ifcondition.py
+++ ifcondition.py
@@ -14,3 +14,32 @@ if(age>10):
    print("True")
 else:
    print("False")
+#eg
+n = 5
+total = 0
+if n != 0:
+    total += n
+    print(total)  
+
+print("This is always executed.")
+
+#nested if else
+
+x = 5
+if x >= 0:
+    if x == 0:
+      print('value is 0')
+    else:
+        print('value is positive')
+else:
+    print('value is negative')
+
+#
+a = 100
+b = 11
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
